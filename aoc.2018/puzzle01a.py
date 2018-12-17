b12144c291bef212f718b9d99bd90829bdffe855
Chuck Staples <cjstaples@gmail.com>
2018-12-17 18:49:05 -0500

    2018 01-a initial solution (no meaningful tests)

--- aoc.2018/puzzle01a.py
+++ aoc.2018/puzzle01a.py
@@ -1,24 +1,42 @@
 import sys
 
-def load_puzzle():
-    # chars = '12131415'
-    chars = '123'
-    return chars
+
+def load_puzzle(input):
+    path = 'data/'
+    filename = input
+    pathname = path + filename
+    with open(pathname, "r") as ins:
+        tmp = ins.read().split("\n")
+        inputlist = [int(i) for i in tmp]
+    arraydata = inputlist
+    return arraydata
+
 
 def solve_puzzle(indata):
     print('-- solving for ' + str(indata))
     final = 0
 
+    for data in indata:
+        final += data
+
     return final
+
 
 def show_puzzle(output):
     print(output)
+
 
 def main():
     print('(puzzle01a) main:')
     print('')
 
-    mydata = load_puzzle()
+    try:
+        param = sys.argv[1]
+    except IndexError:
+        print('** PARAM not found on command line, substitute default test file name**')
+        param = 'aoc-2018_puzzle-1_input_test.txt'
+    print('PARAM: [ ' + param + ' ]')
+    mydata = load_puzzle(param)
 
     mysolution = solve_puzzle(mydata)
     show_puzzle(mysolution)
--- aoc.2018/test/test_p1.py
+++ aoc.2018/test/test_p1.py
@@ -1,1 +1,12 @@
 # test
+def test_sample_01():
+    a = 0
+    assert a == a
+
+def test_sample_02():
+    a = 0
+    assert a == a
+
+def test_sample_03():
+    a = 0
+    assert a == a
