89ce88b84c6b70aeb89f805c6afcc39d159f0d12
Chuck Staples <cstaples@cashstar.com>
2018-12-28 00:54:59 -0500

    puzzle-02 in-progress

--- aoc.2018/puzzle01b.py
+++ aoc.2018/puzzle01b.py
@@ -13,11 +13,33 @@ def load_puzzle(path, input):
 
 def solve_puzzle(indata):
     print('-- solving for ' + str(indata))
+    value = 0
     final = 0
+    frequency = 0
+    freqlist = [0]
 
-    for data in indata:
-        final += data
-
+    while value == 0:
+        for data in indata:
+            print('new data: ',data)
+            frequency += data
+            print('new frequency: ',frequency)
+        # check frequency list to see if it contains the data value
+            if frequency in freqlist:
+                # when duplicate found, set value to 1
+                final = frequency
+                value = 1
+            else:
+                # else add the new value to frequency list
+                freqlist.append(frequency)
+                print('new freqlist: ',sorted(freqlist))
+                # print('len freqlist: ',len(freqlist))
+                # print('min freqlist: ',min(freqlist))
+                # print('max freqlist: ',max(freqlist))
+                print('MIN:',min(freqlist),' :: LEN:',len(freqlist),' :: MAX:',max(freqlist))
+                print('-------------')
+            if value == 1:
+                break
+    # return the first value reached twice
     return final
 
 
