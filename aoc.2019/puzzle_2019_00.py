c7558b5a24c28f9ee2437e0ab50b85cc2d5a2b46
Chuck Staples <cstaples@cashstar.com>
2019-12-04 11:42:46 -0500

    puzzle 3 and 4 boilerplate

---
+++ aoc.2019/puzzle_2019_00.py
@@ -0,0 +1,51 @@
+import sys
+
+
+def load_puzzle(path, puzzle_input):
+    filename = puzzle_input
+    pathname = path + filename
+    with open(pathname, "r") as ins:
+        tmp = ins.read().split("\n")
+        input_set = [int(i) for i in tmp]
+    arraydata = input_set
+    return arraydata
+
+
+def solve_puzzle(indata):
+    print('-- solving for ' + str(indata))
+    final = 0
+
+    return final
+
+
+def show_puzzle(output):
+    print('----')
+    print(output)
+    print('----')
+
+
+def main():
+    print('(puzzle00a) main:')
+    print('')
+
+    try:
+        param = sys.argv[1]
+    except IndexError:
+        print('** PARAM not found on command line, substitute default test file name**')
+        param = 'aoc-2019_puzzle-00_input.txt'
+    print('PARAM: [ ' + param + ' ]')
+    path = 'data/puzzle-00/'
+    mydata = load_puzzle(path, param)
+
+    mysolution = solve_puzzle(mydata)
+    show_puzzle(mysolution)
+    print('')
+
+    print('')
+    print('(puzzle00a) end::')
+
+
+# ----------------------------------------
+if __name__ == '__main__':
+    result = main()
+    sys.exit(result)
---
+++ aoc.2019/test/test_puzzle_2019_p3a.py
@@ -0,0 +1,21 @@
+# test
+import unittest
+
+from puzzle_2019_01a import load_puzzle, solve_puzzle
+
+
+class TestPuzzle2019(unittest.TestCase):
+
+    def test_sample_00(self):
+        mydata = load_puzzle('../data/puzzle-01/', 'aoc-2019_puzzle-03_input_test-0.txt')   #
+        t = solve_puzzle(mydata)
+        assert t == 0
+
+    def test_sample_final_part_1(self):
+        mydata = load_puzzle('../data/puzzle-01/', 'aoc-2019_puzzle-03_input.txt')          # puzzle input
+        t = solve_puzzle(mydata)
+        assert t == 0
+
+
+if __name__ == '__main__':
+    unittest.main()
