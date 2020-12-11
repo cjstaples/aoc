baabd51630e83a9c358a24a9924dbe9ce7dcfb19
Chuck Staples <cjstaples@gmail.com>
2020-12-11 18:30:00 -0500

    2020 puzzle 1 boilerplate - needs work

---
+++ aoc.2020/test/test_puzzle_2020_p1a.py
@@ -0,0 +1,36 @@
+# test
+import unittest
+
+from aoc.2020.puzzle.puzzle_2020_01a import load_puzzle, solve_puzzle
+
+
+class TestPuzzle2020(unittest.TestCase):
+
+    def test_sample_00(self):
+        mydata = load_puzzle('../data/puzzle-01/', 'aoc-2019_puzzle-01_input_test-0.txt')   # 12
+        t = solve_puzzle(mydata)
+        assert t == 2
+
+    def test_sample_01(self):
+        mydata = load_puzzle('../data/puzzle-01/', 'aoc-2019_puzzle-01_input_test-1.txt')   # 14
+        t = solve_puzzle(mydata)
+        assert t == 2
+
+    def test_sample_02(self):
+        mydata = load_puzzle('../data/puzzle-01/', 'aoc-2019_puzzle-01_input_test-2.txt')   # 1969
+        t = solve_puzzle(mydata)
+        assert t == 654
+
+    def test_sample_03(self):
+        mydata = load_puzzle('../data/puzzle-01/', 'aoc-2019_puzzle-01_input_test-3.txt')   # 100756
+        t = solve_puzzle(mydata)
+        assert t == 33583
+
+    def test_sample_final_part_1(self):
+        mydata = load_puzzle('../data/puzzle-01/', 'aoc-2019_puzzle-01_input.txt')          # puzzle input
+        t = solve_puzzle(mydata)
+        assert t == 3406432
+
+
+if __name__ == '__main__':
+    unittest.main()
