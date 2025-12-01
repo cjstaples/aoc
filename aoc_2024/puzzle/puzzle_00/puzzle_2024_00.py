3b3fdf15ce3145f2bf5d3da8c1667c8374cf4427
Chuck Staples <cjstaples@gmail.com>
2025-12-01 17:25:03 -0500

    boilerplate for 2024 / 2025, untested

---
+++ aoc_2024/puzzle/puzzle_00/puzzle_2024_00.py
@@ -0,0 +1,57 @@
+import sys
+
+
+def load_puzzle(path, puzzle_input):
+    filename = puzzle_input
+    pathname = path + filename
+    with open(pathname, "r") as ins:
+        # tmp = ins.read().split("\n")
+        tmp = ins.read().splitlines()
+        # input_set = [int(i) for i in tmp]
+
+    return tmp
+
+
+def solve_puzzle(indata):
+    print('-- solving for ' + str(indata))
+    item_sum = 0
+
+    for data in indata:
+        print('do something with : ' + str(data) + ' ')
+        item_sum = item_sum + int(data)
+    return item_sum
+
+
+# noinspection DuplicatedCode
+def show_puzzle(output):
+    print('----')
+    print(output)
+    print('----')
+
+
+def main():
+    print('(puzzle_2024-00a) main:')
+    print('')
+
+    try:
+        param = sys.argv[1]
+    except IndexError:
+        print('** PARAM not found on command line, substitute default test file name**')
+        param = 'aoc_2024_puzzle_00_input.txt'
+    print(f'PARAM: [ {param} ]')
+    path = 'data/puzzle_00/'
+    mydata = load_puzzle(path, param)
+
+    mysolution = solve_puzzle(mydata)
+    show_puzzle(mysolution)
+    print('')
+
+    print('')
+    print('(puzzle_2024-00a) end::')
+    return 0
+
+
+# ----------------------------------------
+if __name__ == '__main__':
+    result = main()
+    sys.exit(result)
--- aoc_2025/test/test_puzzle_2025_p00.py
+++ aoc_2025/test/test_puzzle_2025_p00.py
@@ -7,12 +7,12 @@ from aoc_2023.puzzle.puzzle_00.puzzle_2023_00 import load_puzzle, solve_puzzle
 class TestPuzzle2023(unittest.TestCase):
 
     def test_sample_00(self):
-        mydata = load_puzzle('../data/puzzle_00/', 'aoc_2024_puzzle_00_input.txt')
+        mydata = load_puzzle('../data/puzzle_00/', 'aoc_2025_puzzle_00_input.txt')
         t = solve_puzzle(mydata)
         assert t == 999
 
     def test_sample_00_test(self):
-        mydata = load_puzzle('../data/puzzle_00/', 'aoc_2024_puzzle_00_input_sample.txt')
+        mydata = load_puzzle('../data/puzzle_00/', 'aoc_2025_puzzle_00_input_sample.txt')
         t = solve_puzzle(mydata)
         assert t == 999
 
